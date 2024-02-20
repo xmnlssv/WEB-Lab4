cfeaee3539c2e0057cbbbf35a9247f99ecd00ae6
xmnlssv <Aakuma1337@gmail.com>
2024-02-20 15:21:10 +0300

    Initial commit of AreaChecker class for geometry calculations

---
+++ src/main/java/com/nameless/lab4/services/AreaChecker.java
@@ -0,0 +1,31 @@
+package com.nameless.lab4.services;
+
+import com.nameless.lab4.entities.Point;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AreaChecker {
+
+    public boolean pointInArea(Point point) {
+        return checkArea(point.getX(), point.getY(), point.getR());
+    }
+
+    public boolean checkArea(Double x, Double y, Double r) {
+        return checkRectangle(x, y, r) || checkTriangle(x, y, r) || checkSector(x, y, r);
+    }
+
+    // First quarter
+    private boolean checkRectangle(Double x, Double y, Double r) {
+        return (x >= 0 && y >= 0 && x <= r && y <= r / 2);
+    }
+
+    // Second quarter
+    private boolean checkTriangle(Double x, Double y, Double r) {
+        return ((x <= 0 && y >= 0 && x >= -r && y <= x + r ));
+    }
+
+    // Four quarter
+    private boolean checkSector(Double x, Double y, Double r) {
+        return ((x >= 0) && (y <= 0) && (Math.pow(x, 2) + Math.pow(y, 2) <= Math.pow(r, 2)));
+    }
+}

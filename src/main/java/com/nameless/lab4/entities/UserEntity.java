2fbfbb1864c664d0d209e69726a50840390b71ee
xmnlssv <Aakuma1337@gmail.com>
2024-02-21 22:15:18 +0300

    Add UserEntity class for user management functionality

---
+++ src/main/java/com/nameless/lab4/entities/UserEntity.java
@@ -0,0 +1,28 @@
+package com.nameless.lab4.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable=false)
+    private String password;
+}

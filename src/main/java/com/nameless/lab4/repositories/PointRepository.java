5761cd42e11f06044a40214a293a974ea71b4f52
xmnlssv <Aakuma1337@gmail.com>
2024-02-21 22:22:50 +0300

    Update repository for Point

--- src/main/java/com/nameless/lab4/repositories/PointRepository.java
+++ src/main/java/com/nameless/lab4/repositories/PointRepository.java
@@ -1,10 +1,13 @@
 package com.nameless.lab4.repositories;
 
-import com.nameless.lab4.entities.Point;
+import com.nameless.lab4.entities.PointEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface PointRepository extends JpaRepository<Point, Integer> {
+import java.util.List;
 
+@Repository
+public interface PointRepository extends JpaRepository<PointEntity, Integer> {
+    void deleteAllByUserId(Integer userId);
+    List<PointEntity> getAllByUserId(Integer userId);
 }

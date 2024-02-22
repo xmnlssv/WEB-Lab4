6caae10788ce19e475e21b092b19b4cbefe1aaaa
xmnlssv <Aakuma1337@gmail.com>
2024-02-23 00:00:44 +0300

    Implement custom UserService with UserDetailsService interface for enhanced authentication

---
+++ src/main/java/com/nameless/lab4/services/UserService.java
@@ -0,0 +1,54 @@
+package com.nameless.lab4.services;
+
+import com.nameless.lab4.entities.UserEntity;
+import com.nameless.lab4.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return (UserDetails) user;
+    }
+
+    public List<UserEntity> allUsers() {
+        return userRepository.findAll();
+    }
+
+    public boolean saveUser(UserEntity user) {
+        UserEntity userFromDB = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDB != null) {
+            return false;
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return true;
+    }
+
+}

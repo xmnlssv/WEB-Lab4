fcfce26733421d51f9fc124eaa6cc3a0d027f5c0
xmnlssv <Aakuma1337@gmail.com>
2024-02-23 00:26:50 +0300

    Change UserRepository findByUsername return type to UserEntity

--- src/main/java/com/nameless/lab4/repositories/UserRepository.java
+++ src/main/java/com/nameless/lab4/repositories/UserRepository.java
@@ -4,5 +4,5 @@ import com.nameless.lab4.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-    boolean existsByUsername(String username);
+    UserEntity findByUsername(String username);
 }

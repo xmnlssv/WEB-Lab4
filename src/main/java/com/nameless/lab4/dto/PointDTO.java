71b8c8324117ec8959a2a57b5280cc96942b98de
xmnlssv <Aakuma1337@gmail.com>
2024-02-22 23:28:04 +0300

    Update values into @Min and @Max

--- src/main/java/com/nameless/lab4/dto/PointDTO.java
+++ src/main/java/com/nameless/lab4/dto/PointDTO.java
@@ -11,18 +11,18 @@ public class PointDTO {
     private Integer id;
 
     @NotNull
-    @Min(value = -3, message = "Min value for X is -3")
+    @Min(value = -5, message = "Min value for X is -5")
     @Max(value = 5, message = "Max value for X is 3")
     private Double x;
 
     @NotNull
-    @Min(value = -3, message = "Min value for Y is -3")
-    @Max(value = 5, message = "Max value for Y is 5")
+    @Min(value = -5, message = "Min value for Y is -5")
+    @Max(value = 3, message = "Max value for Y is 3")
     private Double y;
 
     @NotNull
     @Min(value = 0, message = "Min value for R is 0+")
-    @Max(value = 5, message = "Max value for R is 3")
+    @Max(value = 3, message = "Max value for R is 3")
     private Double r;
 
     private String result;

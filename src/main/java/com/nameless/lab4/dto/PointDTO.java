8949470778b00b5c75e06ef2e5454ad1a1cd92c7
xmnlssv <Aakuma1337@gmail.com>
2024-02-22 14:36:40 +0300

    Implement PointDTO for enhanced data transfer efficiency

---
+++ src/main/java/com/nameless/lab4/dto/PointDTO.java
@@ -0,0 +1,33 @@
+package com.nameless.lab4.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class PointDTO {
+
+    private Integer id;
+
+    @NotNull
+    @Min(value = -3, message = "Min value for X is -3")
+    @Max(value = 5, message = "Max value for X is 3")
+    private Double x;
+
+    @NotNull
+    @Min(value = -3, message = "Min value for Y is -3")
+    @Max(value = 5, message = "Max value for Y is 5")
+    private Double y;
+
+    @NotNull
+    @Min(value = 0, message = "Min value for R is 0+")
+    @Max(value = 5, message = "Max value for R is 3")
+    private Double r;
+
+    private String result;
+
+    private String currentTime;
+
+    private int executionTime;
+}

0f67f449f12416a223955a74b381876ada35e8f2
xmnlssv <Aakuma1337@gmail.com>
2024-02-23 21:56:43 +0300

    Refactor PointService to use DTOs and Mapper for enhanced separation of concerns
    - Implement DTOs for data transfer between service and controller layers.
    - Utilize PointMapper to streamline entity-DTO conversion.

--- src/main/java/com/nameless/lab4/services/PointService.java
+++ src/main/java/com/nameless/lab4/services/PointService.java
@@ -1,28 +1,39 @@
 package com.nameless.lab4.services;
 
-import com.nameless.lab4.entities.Point;
+import com.nameless.lab4.dto.PointRequestDTO;
+import com.nameless.lab4.dto.PointResponseDTO;
+import com.nameless.lab4.entities.PointEntity;
 import com.nameless.lab4.repositories.PointRepository;
+import com.nameless.lab4.utils.PointMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PointService {
 
     private final PointRepository pointRepository;
+    private final PointMapper pointMapper;
 
     @Autowired
-    public PointService(PointRepository pointRepository) {
+    public PointService(PointRepository pointRepository, PointMapper pointMapper) {
         this.pointRepository = pointRepository;
+        this.pointMapper = pointMapper;
     }
 
-    public List<Point> findAllPoints() {
-        return pointRepository.findAll();
+    public List<PointResponseDTO> findAllPoints() {
+        return pointRepository.findAll().stream()
+                .map(pointMapper::mapToPointDTO)
+                .collect(Collectors.toList());
     }
 
-    public Point checkAndSavePoint(Point point) {
-        return pointRepository.save(point);
+    public PointResponseDTO addPoint(PointRequestDTO pointRequestDTO) {
+        PointEntity pointEntity = pointMapper.mapToPointEntity(pointRequestDTO);
+        PointEntity savedPoint = pointRepository.save(pointEntity);
+        return pointMapper.mapToPointDTO(savedPoint);
+
     }
 
     public void deletePoints() {

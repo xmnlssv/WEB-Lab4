5fceb57636bb612964994246738383568bf3bde6
xmnlssv <Aakuma1337@gmail.com>
2024-02-26 20:18:36 +0300

    Added classes for handling user authentication and registration
    - JwtRequest and UserDTO are used for authentication purposes and, although they have identical structures, are named differently to semantically separate the contexts in which they are used.
    - JwtResponse provides a structure for responding with a JWT token.
    - RegistrationUser is added to handle new user registration data.

---
+++ src/main/java/com/nameless/lab4/dto/User.java
@@ -0,0 +1,11 @@
+package com.nameless.lab4.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private Long id;
+    private String username;
+}

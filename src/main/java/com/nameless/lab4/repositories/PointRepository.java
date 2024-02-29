7fd84039fe18c777ab309a5c40ca620505698668
xmnlssv <Aakuma1337@gmail.com>
2024-02-29 13:41:34 +0300

    Remove methods

--- src/main/java/com/nameless/lab4/repositories/PointRepository.java
+++ src/main/java/com/nameless/lab4/repositories/PointRepository.java
@@ -4,10 +4,6 @@ import com.nameless.lab4.entities.PointEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface PointRepository extends JpaRepository<PointEntity, Integer> {
-    void deleteAllByUserId(Integer userId);
-    List<PointEntity> getAllByUserId(Integer userId);
 }

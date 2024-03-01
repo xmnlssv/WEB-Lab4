2f9ea2e0c78ed4a6ea7d33fca4619178334de4b8
xmnlssv <Aakuma1337@gmail.com>
2024-03-01 20:05:27 +0300

    Refactor UserService and add user registration
    Refactored UserService to use Optional in findByUsername for cleaner error handling. Added createNewUser method to handle user registration, utilizing BCryptPasswordEncoder for password security.

--- src/main/java/com/nameless/lab4/services/UserService.java
+++ src/main/java/com/nameless/lab4/services/UserService.java
@@ -1,54 +1,48 @@
 package com.nameless.lab4.services;
 
+import com.nameless.lab4.dto.RegistrationUser;
 import com.nameless.lab4.entities.UserEntity;
 import com.nameless.lab4.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.Collections;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
 
-    @Autowired
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+
+    public Optional<UserEntity> findByUsername(String username) {
+        return userRepository.findByUsername(username);
     }
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity user = userRepository.findByUsername(username);
-
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found");
-        }
-
-        return (UserDetails) user;
+        UserEntity user = findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(
+                String.format("Пользователь '%s' не найден", username)
+        ));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
     }
 
-    public List<UserEntity> allUsers() {
-        return userRepository.findAll();
+    public UserEntity createNewUser(RegistrationUser registrationUser) {
+        UserEntity user = new UserEntity();
+        user.setUsername(registrationUser.getUsername());
+        user.setPassword(passwordEncoder.encode(registrationUser.getPassword()));
+        return userRepository.save(user);
     }
-
-    public boolean saveUser(UserEntity user) {
-        UserEntity userFromDB = userRepository.findByUsername(user.getUsername());
-
-        if (userFromDB != null) {
-            return false;
-        }
-
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRepository.save(user);
-        return true;
-    }
-
 }

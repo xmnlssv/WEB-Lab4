e8473d99a0ce152f2df29cfd184a64ad49d08404
xmnlssv <Aakuma1337@gmail.com>
2024-03-01 20:10:07 +0300

    Implement JwtRequestFilter for JWT token validation
    Created JwtRequestFilter to extract and validate JWT tokens from the Authorization header. This filter checks for token expiration, signature validity, and sets the authentication in the security context if the token is valid.

---
+++ src/main/java/com/nameless/lab4/config/JwtRequestFilter.java
@@ -0,0 +1,52 @@
+package com.nameless.lab4.config;
+
+import com.nameless.lab4.utils.JwtToken;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtRequestFilter extends OncePerRequestFilter {
+    private final JwtToken jwtToken;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        String username = null;
+        String jwt = null;
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            jwt = authHeader.substring(7);
+            try {
+                username = jwtToken.getUsername(jwt);
+            } catch (ExpiredJwtException e) {
+                log.debug("Время жизни токена вышло");
+            } catch (SignatureException e) {
+                log.debug("Подпись неправильная");
+            }
+        }
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                    username,
+                    null
+            );
+            SecurityContextHolder.getContext().setAuthentication(token);
+        }
+        filterChain.doFilter(request, response);
+    }
+}

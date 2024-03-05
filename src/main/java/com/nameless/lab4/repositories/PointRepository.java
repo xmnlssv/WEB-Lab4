8a2485aa1a90c08ac28513fa4b6cc4156b381746
xmnlssv <Aakuma1337@gmail.com>
2024-03-06 01:03:20 +0300

    Add methods for user-specific data handler

--- src/main/java/com/nameless/lab4/repositories/PointRepository.java
+++ src/main/java/com/nameless/lab4/repositories/PointRepository.java
@@ -1,9 +1,17 @@
 package com.nameless.lab4.repositories;
 
+import com.nameless.lab4.dto.User;
 import com.nameless.lab4.entities.PointEntity;
+import com.nameless.lab4.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PointRepository extends JpaRepository<PointEntity, Integer> {
+    List<PointEntity> findAllByUserId(UserEntity user);
+
+    void deleteAllByUserId(UserEntity user);
 }

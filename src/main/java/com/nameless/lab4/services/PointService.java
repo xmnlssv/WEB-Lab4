ba45973fcf205d4d09fc765c7f9494b3ceeeffde
xmnlssv <Aakuma1337@gmail.com>
2024-03-06 01:05:58 +0300

    Enhance PointService to handle user-specific point operations
    - Integrate UserRepository to fetch user-specific data based on the username.
    - Modify findAllPoints, addPoint, and deletePoints methods to support operations based on the authenticated user's context.
    - Utilize @Transactional on deletePoints to ensure consistency during deletion process.
    - Adapte point operations to use user-based filtering for personalizing data access and manipulation.

--- src/main/java/com/nameless/lab4/services/PointService.java
+++ src/main/java/com/nameless/lab4/services/PointService.java
@@ -3,9 +3,13 @@ package com.nameless.lab4.services;
 import com.nameless.lab4.dto.PointRequest;
 import com.nameless.lab4.dto.PointResponse;
 import com.nameless.lab4.entities.PointEntity;
+import com.nameless.lab4.entities.UserEntity;
 import com.nameless.lab4.repositories.PointRepository;
+import com.nameless.lab4.repositories.UserRepository;
 import com.nameless.lab4.utils.PointMapper;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,28 +19,35 @@ import java.util.stream.Collectors;
 public class PointService {
 
     private final PointRepository pointRepository;
+    private final UserRepository userRepository;
     private final PointMapper pointMapper;
 
     @Autowired
-    public PointService(PointRepository pointRepository, PointMapper pointMapper) {
+    public PointService(PointRepository pointRepository, PointMapper pointMapper, UserRepository userRepository) {
         this.pointRepository = pointRepository;
         this.pointMapper = pointMapper;
+        this.userRepository = userRepository;
     }
 
-    public List<PointResponse> findAllPoints() {
-        return pointRepository.findAll().stream()
+    public List<PointResponse> findAllPoints(String username) {
+        UserEntity user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
+        return pointRepository.findAllByUserId(user).stream()
                 .map(pointMapper::mapToPointDTO)
                 .collect(Collectors.toList());
     }
 
-    public PointResponse addPoint(PointRequest pointRequestDTO) {
+    public PointResponse addPoint(PointRequest pointRequestDTO, String username) {
+        UserEntity user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
         PointEntity pointEntity = pointMapper.mapToPointEntity(pointRequestDTO);
+        pointEntity.setUserId(user);
         PointEntity savedPoint = pointRepository.save(pointEntity);
         return pointMapper.mapToPointDTO(savedPoint);
-
     }
 
-    public void deletePoints() {
-        pointRepository.deleteAll();
+    @Transactional
+    public void deletePoints(String username) {
+        userRepository.findByUsername(username).ifPresent(pointRepository::deleteAllByUserId);
     }
 }

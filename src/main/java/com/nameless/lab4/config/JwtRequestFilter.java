957686d2c2cfd45283f901d6b975732e8fdf8782
xmnlssv <Aakuma1337@gmail.com>
2024-03-06 01:45:09 +0300

    Delete log

--- src/main/java/com/nameless/lab4/config/JwtRequestFilter.java
+++ src/main/java/com/nameless/lab4/config/JwtRequestFilter.java
@@ -8,7 +8,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -19,7 +18,6 @@ import java.util.Collections;
 
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class JwtRequestFilter extends OncePerRequestFilter {
     private final JwtToken jwtToken;
 
